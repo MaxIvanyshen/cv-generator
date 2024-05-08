acf4c698d8d6700ae1c8537e6c46ff98008de7f5
MaxIvanyshen <maxivanyshen@gmail.com>
2024-05-08 16:39:17 +0300

    add proper data file input handling

--- main.go
+++ main.go
@@ -4,15 +4,27 @@ import (
 	"cv/args"
 	"cv/cv"
 	"fmt"
+	"strings"
 )
 
 func main() {
     args := args.ParseCreateArgs()
 
-    cv, err := cv.ParseFromYaml(args.DataFile)
+    var resume cv.CV 
+    var err error
+
+    fileType := strings.Split(args.DataFile, ".")[1]
+    if fileType == "json" {
+        resume, err = cv.ParseFromJson(args.DataFile)
+    } else if fileType == "yml" {
+        resume, err = cv.ParseFromYaml(args.DataFile)
+    } else {
+        err = fmt.Errorf("unsupported file type: '.%s'. Use json or yaml", fileType)
+    }
+
     if err != nil {
         fmt.Println(err)
     }
 
-    fmt.Println(cv)
+    fmt.Println(resume)
 }
